0f5c892d0751be1fcfdf0c937789ae198ecdc70a
bogdanpasterak <bogdanpasterak@gmail.com>
2020-04-27 14:16:50 +0100

    add http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartPageComponent } from './start-page/start-page.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { StartPageComponent } from './start-page/start-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
